dc0ada8c2ca7287223c9b84ec4604396e86c7a59
Jack Giffin <25795277+anonyco@users.noreply.github.com>
2018-02-17 10:15:38 -0500

    Add files via upload

--- DeferStack.src.js
+++ DeferStack.src.js
@@ -6,44 +6,55 @@
 	const DEBUGMODE = true;
 	///////////////////////
 	const persuadoStack = []; // Defered loading of promises to outermost promise scope so as to not exceed the stack level
+	const transPersuadoStack = []; // temporary buffer for the persuado stack
 	const promiseBufferLvl = 256; // length of the promise buffer for double stack buffering
 	var deferLevel = 0;
+	var isAtHigherLvl = false;
 	var isInsideDeferable = false; // because javascript is single threaded, we can use this to determine if there is an outermost promise
 	var pStackLength = 0;		 // the current size of the persuadoStack
+	var previous
+	/** @noCollapse */
 	window["DeferStack"] = function(func, start){
-		start = +start || 1;
+		if (DEBUGMODE){
+			if (typeof func !== "function") console.error("The first argument to DeferStack must be a function.");
+			if (start !== undefined && typeof start !== "number") console.error("The second argument to DeferStack must be a number or undefined (for the default).");
+		}
 		if (isInsideDeferable) {
-			persuadoStack.push(func);
+			transPersuadoStack.unshift(func);
 			++pStackLength;
 		} else {
-			if (deferLevel >= promiseBufferLvl){
-				isInsideDeferable = true;
-				persuadoStack.push(func);
-				++pStackLength;
-			} else {
-				if (deferLevel)
+			if (isAtHigherLvl) {
+				if (deferLevel >= promiseBufferLvl){
+					isInsideDeferable = true;
+					transPersuadoStack.unshift(func);
+					++pStackLength;
+				} else {
 					++deferLevel;
-				else
-					deferLevel = start;
+					func();
+				}
+			} else {
+				deferLevel = start || 1;
+				isAtHigherLvl = true;
 				func();
-				if (deferLevel === start) { // if this is the outermost promise
+				if (isInsideDeferable) { // if this is the outermost promise
 					try {
-						if (pStackLength) {
-							do {
+						do {
+							deferLevel = start || 1;
+							if (isInsideDeferable) {
+								// if some stuff got added onto the transPersuadoStack
+								persuadoStack.push.apply(persuadoStack, transPersuadoStack);
+								transPersuadoStack.length = 0;
 								isInsideDeferable = false;
-								persuadoStack.shift()();
-							} while (--pStackLength);
-						}
-						isInsideDeferable = false;
-						deferLevel = 0;
+							}
+							persuadoStack.pop()();
+						} while (--pStackLength);
 					} catch(e) {
 						// reset everything so that the error doesn't permanently ruin everything
-						isInsideDeferable = false;
-						deferLevel = 0;
-						persuadoStack.length = pStackLength = 0;
-						throw e;
+						if (DEBUGMODE) console.error(e);
 					}
-				} else --deferLevel;
+				}
+				isInsideDeferable = isAtHigherLvl = false;
+				deferLevel = persuadoStack.length = pStackLength = 0;
 			}
 		}
 	}
